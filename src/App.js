6af757941816535c53af33daa62d99e6d330a8ae
stepankniazevich <steoan2000kniaz@outlook.com>
2022-01-26 00:24:32 +0200

    add gt pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,30 @@ import { Formik } from 'formik'
 
 function App() {
 
+  const validationsSchema = yup.object().shape({
+
+    name: yup.string().typeError("Error, should be line ").required(),
+
+    secondName: yup.string().typeError("Error, should be line ").required(),
+
+    password: yup.string()
+      .min(8, 'too short password')
+      .max(16, 'too long password')
+      .required()
+      .matches(/[0-9]/, 'Password can only contain number.'),
+
+    confirmPassword: yup.string()
+      .min(8, 'too short password')
+      .max(16, 'too long password')
+      .required()
+      .matches(/[0-9]/, 'Password can only contain number.')
+      .oneOf([yup.ref('password'), null], 'Passwords must match'),
+
+    email: yup.string()
+      .email('Invalid email').required('Required')
+
+  })
+
 
 
   return (
@@ -21,10 +45,12 @@ function App() {
         }}
         validateOnBlur
         onSubmit={(values) => { console.log(values) }}
+        validationSchema={validationsSchema}
       >
         {({ values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
           <div className={"form"}>
             <form>
+              <label htmlFor={"name"}>{"Login: "}</label><br />
               <input
                 className={'input'}
                 type={"text"}
@@ -33,7 +59,39 @@ function App() {
                 onBlur={handleBlur}
                 value={values.name}
               />
-              {touched.name && errors.name && <p className={"error"}>{'Помилка'}</p>}
+              {touched.name && errors.name && <span className="error">{' *введіть щось'}</span>} <br />
+
+              <label htmlFor={"email"}>{"Email: "}</label><br />
+              <input
+                className={'input'}
+                type={"email"}
+                name={"email"}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              {touched.email && errors.email && <span className="error">{errors.email}</span>}
+              <br />
+              <label htmlFor={"password"}>{"Password: "}</label><br />
+              <input
+                className={'input'}
+                type={"password"}
+                name={"password"}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+              {touched.password && errors.password && <span className="error">{errors.password}</span>}<br />
+              <label htmlFor={"confirmPassword"}>{"Confirm password: "}</label><br />
+              <input
+                className={'input'}
+                type={"password"}
+                name={"confirmPassword"}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.confirmPassword}
+              />
+              {touched.confirmPassword && errors.confirmPassword && <span className="error">{errors.confirmPassword}</span>}
               <br />
               <button
                 disabled={!isValid && !dirty}
